255fa27c14a63edc971d7787b5354c0c09abb966
Michael Kenney <michael.kenney@returnpath.com>
2017-12-02 14:39:03 -0700

    update deployment

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-if [ "false" != "$TRAVIS_PULL_REQUEST" ]; then
+echo "TRAVIS_PULL_REQUEST: '$TRAVIS_PULL_REQUEST'"
+if [ "false" = "$TRAVIS_PULL_REQUEST" ]; then
     echo "Triggering builds for branch '$TRAVIS_BRANCH'"
     curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$TRAVIS_BRANCH\"}" -X POST https://registry.hub.docker.com/u/mkenney/php-base/trigger/$DOCKER_TOKEN/
 fi
